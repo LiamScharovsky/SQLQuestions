a99495cf288393fb5f0becea3fe2ef4686f9ce08
LiamScharovsky <99295834+LiamScharovsky@users.noreply.github.com>
2022-04-26 10:43:07 -0500

    Add files via upload

--- sqlQuestions.sql
+++ sqlQuestions.sql
@@ -1,12 +1,12 @@
 --1.Find how many payment transactions were greater
 --than $5.00
-select * 
+select count(amount) 
 from payment
 where amount > 5
 
 --2.How many actors have a name that starts with the
 --letter P?
-select * 
+select count (actor) 
 from actor 
 where first_name like 'P%'
 
@@ -21,13 +21,13 @@ from address
 
 --5.How many films are rated R and have a replacement
 --cost between $5 and $15?
-select *
+select count(film)
 from film
 where rating = 'R' and replacement_cost < 15 and replacement_cost > 5
 
 --6.How many films have the word ‘Truman’ somewhere
 --in the title?
-select *
+select count(film)
 from film
 where title like '%Truman%'
 
@@ -39,7 +39,7 @@ where amount > 7.99
 
 --8.How many actors start with the letter D and have at
 --least 3 characters in their name?
-select * 
+select count(actor) 
 from actor 
 where first_name like 'D___%'
 
